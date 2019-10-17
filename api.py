012bbf8b199db5100e19398197272982ecb2a917
Rodrigo Monte <rodrigo.monte@onefootball.com>
2019-10-17 09:43:39 +0200

    Refactor to remove flask-restplus framework

--- api.py
+++ api.py
@@ -1,39 +1,17 @@
 import os
-from flask import Flask
-from flask_restplus import Resource, Api, fields
+import connexion
+from flask import request
 from model import Model
 
-app = Flask(__name__)
-
-api = Api(app, version='1.0', title='Titanic API',
-          description='A simple Titanic API to preditct survivers')
-
-ns = api.namespace('titanic', description='Predict Titanic survivers')
-
-
-features = api.model('features', {
-    'Pclass': fields.Integer(required=True,
-                             description='Ticket class',
-                             example=3),
-    'Parch': fields.Integer(required=True,
-                            description='parents/children aboard the Titanic',
-                            example=0),
-    'Age': fields.Integer(required=True, description='Age', example=33),
-    'Sex': fields.String(required=True, description='Sex', example='female')
-})
 
 clf = Model()
 
-@api.route('/predict')
-class Predict(Resource):
+def predict():
+    body = request.json
+    return clf.predict([body])
 
-    @ns.doc('predict')
-    @ns.expect(features)
-    @ns.response(200, '0 or 1')
-    def post(self):
-
-        return clf.predict([api.payload])
-
+app = connexion.FlaskApp(__name__, specification_dir='openapi/')
+app.add_api('openapi.yaml')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
